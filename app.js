3bac8c6fddfa85baa1047cb96b2558a5cd5b04f4
Dmytro <ljutyjj.dima@hotmail.com>
2021-11-05 13:05:51 +0200

    add live-reload

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+const path = require("path");
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
+
+const livereloadServer = livereload.createServer();
+livereloadServer.watch(path.join(__dirname, 'public'));
+
+livereloadServer.server.once("connection", () => {
+    setTimeout(() => {
+        livereloadServer.refresh("/");
+    }, 100);
+});
+
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(connectLivereload());
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(port, () => {
